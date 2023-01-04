9afe4962b64a0cb1c5fbaeed2cc35091d90691d7
AJ-Derteano <andresderteano@outlook.com>
2023-01-03 22:41:20 -0500

    Add lang in local storage

--- src/context/LangContext.jsx
+++ src/context/LangContext.jsx
@@ -6,24 +6,48 @@ import MessageES from "./../lang/es-PE.json";
 const LangContext = React.createContext();
 
 const LangProvider = ({ children }) => {
-  const [message, setMessage] = useState(MessageES);
-  const [locale, setLocale] = useState("es-PE");
+  let defaultLocale;
+  let defaultMessage;
+
+  const lang = localStorage.getItem("lang");
+
+  if (lang) {
+    defaultLocale = lang;
+
+    if (lang === "es-PE") {
+      defaultMessage = MessageES;
+    } else if (lang === "en-US") {
+      defaultMessage = MessageEN;
+    } else {
+      defaultLocale = "es-PE";
+      defaultMessage = MessageES;
+    }
+  } else {
+    defaultLocale = "es-PE";
+    defaultMessage = MessageES;
+  }
+
+  const [message, setMessage] = useState(defaultMessage);
+  const [locale, setLocale] = useState(defaultLocale);
 
   const handleLanguage = (language) => {
     switch (language) {
       case "es-PE":
         setMessage(MessageES);
         setLocale("es-PE");
+        localStorage.setItem("lang", "es-PE");
         break;
 
       case "en-US":
         setMessage(MessageEN);
         setLocale("en-US");
+        localStorage.setItem("lang", "en-US");
         break;
 
       default:
         setMessage(MessageES);
         setLocale("es-PE");
+        localStorage.setItem("lang", "es-PE");
     }
   };
 
