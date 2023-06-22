1e6a34a11e3cd8aaf90ad2a8365a56c4d646ffce
AJ-Derteano <andresderteano@outlook.com>
2023-06-21 21:57:17 -0500

    Add dark mone

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ConfigProvider, FloatButton, Tooltip, theme } from 'antd';
+import esES from 'antd/locale/es_ES';
 import './App.css';
 import {
   CustomContent,
@@ -7,16 +10,29 @@ import {
   CustomNavbar,
   CustomSider,
 } from '@/common';
+import { FormatPainterOutlined, PlusOutlined } from '@ant-design/icons';
+
+const { darkAlgorithm, defaultAlgorithm } = theme;
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
-    <>
+    <ConfigProvider
+      locale={esES}
+      theme={{
+        algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+        token: {
+          colorPrimary: '#5bc0de',
+        },
+      }}
+    >
       <BrowserRouter>
         <Routes>
           <Route
             path='/'
             element={
               <CustomLayout
+                darkMode={isDarkMode}
                 sider={<CustomSider />}
                 header={<CustomNavbar />}
                 content={<CustomContent />}
@@ -26,7 +42,23 @@ function App() {
           />
         </Routes>
       </BrowserRouter>
-    </>
+      <FloatButton.Group
+        trigger='hover'
+        type='primary'
+        style={{ right: 24 }}
+        icon={<PlusOutlined />}
+      >
+        <Tooltip
+          title={isDarkMode ? 'Modo claro' : 'Modo oscuro'}
+          placement='left'
+        >
+          <FloatButton
+            icon={<FormatPainterOutlined />}
+            onClick={() => setIsDarkMode((previus) => !previus)}
+          />
+        </Tooltip>
+      </FloatButton.Group>
+    </ConfigProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,7 @@
 import ReactDOM from 'react-dom/client';
-import { ConfigProvider } from 'antd';
-import esES from 'antd/locale/es_ES';
 import App from './App.tsx';
 import 'normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ConfigProvider
-    locale={esES}
-    theme={{
-      token: {
-        colorPrimary: '#5bc0de',
-      },
-    }}
-  >
-    <App />
-  </ConfigProvider>
+  <App />
 );
