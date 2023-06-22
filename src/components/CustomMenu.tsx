f54ccc6aa0d82da6d8186f152194c1938e8fb9a4
AJ-Derteano <andresderteano@outlook.com>
2023-06-21 21:57:26 -0500

    add options

--- src/components/CustomMenu.tsx
+++ src/components/CustomMenu.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Menu, MenuProps } from 'antd';
+import { Menu, MenuProps, Typography } from 'antd';
+import {
+  AppstoreOutlined,
+  ReadOutlined,
+  ThunderboltOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
 import styled from 'styled-components';
-import { PieChartOutlined } from '@ant-design/icons';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
 const CustomMenu: React.FC = () => {
   function getItem(
-    label: React.ReactNode,
+    label: string,
     key: React.Key,
     icon?: React.ReactNode,
     children?: MenuItem[],
@@ -17,13 +22,26 @@ const CustomMenu: React.FC = () => {
       key,
       icon,
       children,
-      label,
+      label: (
+        <Typography.Text style={{ fontSize: 16 }}>{label}</Typography.Text>
+      ),
       type,
     } as MenuItem;
   }
 
   const items: MenuProps['items'] = [
-    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Sobre mi', '1', <UserOutlined style={{ fontSize: 16 }} />),
+    getItem('Conocimientos', '1', <ReadOutlined style={{ fontSize: 16 }} />),
+    getItem(
+      'Habilidades',
+      '1',
+      <ThunderboltOutlined style={{ fontSize: 16 }} />
+    ),
+    getItem(
+      'Mis proyectos',
+      '1',
+      <AppstoreOutlined style={{ fontSize: 16 }} />
+    ),
   ];
 
   return (
@@ -31,8 +49,9 @@ const CustomMenu: React.FC = () => {
       <Menu items={items} />
     </CustomMenuStyle>
   );
-};
+}; //<ReadOutlined /> <ThunderboltOutlined /> <AppstoreOutlined />
 
 export const CustomMenuStyle = styled.div``;
 
 export default CustomMenu;
+
